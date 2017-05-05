38beb686d4b7256663d1cdd3ccfd1d9d87dc5a24
Marcin Wojdyr <wojdyr@gmail.com>
2017-05-05 20:16:47 +0100

    tests/compare_pdb.sh: script for comparing converted PDB with PDB PDB.

---
+++ tests/compare_pdb.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Index with dates helps limit the testing given time span:
+#  ftp://ftp.wwpdb.org/pub/pdb/derived_data/index/entries.idx
+# tail -n +3 entries.idx | cut -f1,3 | grep "05/16$" |\
+# while read -r code date; do ./compare_pdb.sh $code; done
+
+set -eu
+cd `dirname $0`
+code=${1,,}
+pout="p/$code"
+gout="g/$code"
+cif=${PDB_COPY:-/hdd}/mmCIF/${code:1:2}/${code}.cif.gz
+pdb=${PDB_COPY:-/hdd}/pdb/${code:1:2}/pdb${code}.ent.gz
+ignore="\
+^AUTHOR|\
+^CISPEP|\
+^COMPND|\
+^CONECT|\
+^DBREF |\
+^EXPDTA|\
+^FORMUL|\
+^HELIX |\
+^HET   |\
+^HETNAM|\
+^HETSYN|\
+^JRNL  |\
+^KEYWDS|\
+^LINK  |\
+^MASTER|\
+^MTRIX|\
+^MODRES|\
+^ORIGX|\
+^REMARK|\
+^REVDAT|\
+^SCALE|\
+^SEQADV|\
+^SEQRES|\
+^SHEET |\
+^SITE  |\
+^SOURCE|\
+^SPRSDE|\
+^SSBOND"
+#zgrep -v -f ignore "$pdb" > "$pout"
+zgrep -v -E "$ignore" "$pdb" > "$pout"
+../gemmi-convert --to=pdb "$cif" "$gout"
+echo diff -u "$gout" "$pout"
+diff -u "$gout" "$pout" | diffstat -q
+
+# Add d or w as the second arg to show diff (using git diff for colors).
+[[ ${2:-} = d ]] && git_diff_opt=
+[[ ${2:-} = w ]] && git_diff_opt=--word-diff=color
+if [[ "${git_diff_opt+set}" ]]; then
+  echo git diff --no-index $git_diff_opt -- "$gout" "$pout"
+  git diff --no-index $git_diff_opt -- "$gout" "$pout"
+fi
