f0e56916f399e473e67ebaad424036fd1fe37a12
Marcin Wojdyr <wojdyr@gmail.com>
2017-05-24 17:26:46 +0100

    add example that checks column order in _atom_site

---
+++ examples/col_order.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from gemmi import cif
+from util import get_file_paths_from_args
+
+# Check column presence and order in the _atom_site category.
+# In mmCIF v5 esd _atom_site.*esd columns were removed.
+ESD = 'Cartn_x_esd Cartn_y_esd Cartn_z_esd occupancy_esd B_iso_or_equiv_esd '
+#ESD = ''
+USUAL_ORDER = ('group_PDB id type_symbol label_atom_id label_alt_id '
+               'label_comp_id label_asym_id label_entity_id label_seq_id '
+               'pdbx_PDB_ins_code Cartn_x Cartn_y Cartn_z occupancy '
+               'B_iso_or_equiv ' + ESD + 'pdbx_formal_charge '
+               'auth_seq_id auth_comp_id auth_asym_id auth_atom_id '
+               'pdbx_PDB_model_num')
+counts = {}
+for path in get_file_paths_from_args():
+    block = cif.read_any(path).sole_block()
+    loop_tags = block.find_loop("_atom_site.id").loop.tags
+    assert all(t.tag.startswith("_atom_site.") for t in loop_tags)
+    tags = ' '.join(t.tag[11:] for t in loop_tags)
+    if tags != USUAL_ORDER:
+        print(tags)
+        print(USUAL_ORDER)
+        print(block.name, tags)
+        counts[tags] = counts.get(tags, 0) + 1
+
+for key, value in counts.items():
+    print(value, key)
+
+# Results: in v4 a few EM structures (5A9Z 5AA0 5FKI 4UDF)
+# had different order, with ATOM/HETATM in the middle.
