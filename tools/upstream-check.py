0058ef1a7193c6561210350729ac0bf9895ee4aa
Marcin Wojdyr <wojdyr@gmail.com>
2017-09-04 18:18:34 +0100

    upstream-check.py: catch network error

--- tools/upstream-check.py
+++ tools/upstream-check.py
@@ -19,27 +19,35 @@ NOT_TAGGED_REPOS = {
     'nothings/stb': 'stb_sprintf.h',
 }
 
-for repo, version in TAGGED_REPOS.items():
-    url = 'https://api.github.com/repos/%s/releases/latest' % repo
-    response = urllib.urlopen(url)
-    data = json.load(response)
-    if 'tag_name' in data:
-        latest_tag = data['tag_name']
-    else:
-        url = 'https://api.github.com/repos/%s/tags' % repo
+def check_tags():
+    for repo, version in TAGGED_REPOS.items():
+        url = 'https://api.github.com/repos/%s/releases/latest' % repo
         response = urllib.urlopen(url)
         data = json.load(response)
-        latest_tag = data[0]['name']
-    mark = ('   !!!' if version != latest_tag else '')
-    print('%-18s %10s %10s%s' % (repo, version, latest_tag, mark))
+        if 'tag_name' in data:
+            latest_tag = data['tag_name']
+        else:
+            url = 'https://api.github.com/repos/%s/tags' % repo
+            response = urllib.urlopen(url)
+            data = json.load(response)
+            latest_tag = data[0]['name']
+        mark = ('   !!!' if version != latest_tag else '')
+        print('%-18s %10s %10s%s' % (repo, version, latest_tag, mark))
 
-since = datetime.datetime.now() - datetime.timedelta(days=30)
-for repo, filename in NOT_TAGGED_REPOS.items():
-    url = 'https://api.github.com/repos/%s/commits?path=%s&since=%s' % (
-            repo, filename, since)
-    response = urllib.urlopen(url)
-    data = json.load(response)
-    info = '-'
-    if data:
-        info = data[0]['commit']['committer']['date'][:10]
-    print('%-18s  modified: %s' % (repo, info))
+def check_recent_commits():
+    since = datetime.datetime.now() - datetime.timedelta(days=30)
+    for repo, filename in NOT_TAGGED_REPOS.items():
+        url = 'https://api.github.com/repos/%s/commits?path=%s&since=%s' % (
+                repo, filename, since)
+        response = urllib.urlopen(url)
+        data = json.load(response)
+        info = '-'
+        if data:
+            info = data[0]['commit']['committer']['date'][:10]
+        print('%-18s  modified: %s' % (repo, info))
+
+try:
+    check_tags()
+    check_recent_commits()
+except IOError as e:
+    print(e)
