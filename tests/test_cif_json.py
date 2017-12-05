88e7dc1e9456f830da77259cede127460aba7b55
Marcin Wojdyr <wojdyr@gmail.com>
2017-12-05 11:03:54 +0000

    add a cif file with unicode name for testing

--- tests/test_cif_json.py
+++ tests/test_cif_json.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
 import io
 import json
 import unittest
 
-import gemmi
+from gemmi import cif
 
 def nonempty_lines(s):
     return [line for line in s.splitlines() if line and line[0] != '#']
@@ -14,7 +15,7 @@ class TestCifAsJson(unittest.TestCase):
     def setUp(self):
         self.basename = os.path.join(os.path.dirname(__file__), 'misc')
     def test_misc(self):
-        cif_doc = gemmi.cif.read_file(self.basename + '.cif')
+        cif_doc = cif.read_file(self.basename + '.cif')
         json_str = cif_doc.as_json()
         json_from_cif = json.loads(json_str)
         with io.open(self.basename + '.json', encoding='utf-8') as f:
@@ -24,7 +25,7 @@ class TestCifAsJson(unittest.TestCase):
     def test_cif_as_string(self):
         with open(self.basename + '.cif', 'rb') as f:
             cif_orig = f.read().decode('utf-8').replace('\r\n', '\n')
-        cif_doc = gemmi.cif.read_string(cif_orig)
+        cif_doc = cif.read_string(cif_orig)
         formatting_changes = {
             '# comment\n': '',
             '_x _y _z': '_x\n_y\n_z',
@@ -35,6 +36,13 @@ class TestCifAsJson(unittest.TestCase):
             cif_orig = cif_orig.replace(k, v)
         cif_out = cif_doc.as_string()
         self.assertListEqual(nonempty_lines(cif_orig), nonempty_lines(cif_out))
+
+class TestSpecialFilename(unittest.TestCase):
+    def test_open(self):
+        if os.name == 'nt': return
+        path = os.path.join(os.path.dirname(__file__), u'gęś¼.cif')
+        doc = cif.read_file(path)
+        self.assertEqual(doc[0].find_value('_tricky'), 'filename')
 
 if __name__ == '__main__':
     unittest.main()
