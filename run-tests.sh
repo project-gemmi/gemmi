eed0403789cfb8697e2b53aa06b1b2133bad29a1
Marcin Wojdyr <wojdyr@gmail.com>
2018-01-29 15:06:57 +0000

    cmake: optionally build python module

--- run-tests.sh
+++ run-tests.sh
@@ -3,9 +3,12 @@
 set -eu
 cd "$(dirname "$0")"
 (cd src && make -j4 all write-help)
-(cd python && make -j4)
+BUILD_DIR="."
+[ -e build ] && BUILD_DIR=build
+(cd $BUILD_DIR && make -j4 all check)
 (cd docs && make -j4 html SPHINXOPTS="-q -n")
-PYTHONPATH=python python -m unittest discover -s tests
+export PYTHONPATH=$BUILD_DIR
+python3 -m unittest discover -s tests
 # Where python 2 and 3 output differ, the docs have output from v3.
 # So 'make doctest' works only if sphinx-build was installed for python3.
 (cd docs && make doctest SPHINXOPTS="-q -n")
