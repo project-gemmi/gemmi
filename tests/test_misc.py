707dee99d30487746d581da34af9fa05b45b327a
Marcin Wojdyr <wojdyr@gmail.com>
2019-06-27 18:16:28 +0200

    fix test failing on Windows

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -8,12 +8,14 @@ class TestMisc(unittest.TestCase):
     def test_pdb_code(self):
         self.assertTrue(gemmi.is_pdb_code('9ALA'))
         self.assertFalse(gemmi.is_pdb_code('BAD1'))
-        os.environ['PDB_DIR'] = '/data'
-        cif_4xyz = '/data/structures/divided/mmCIF/xy/4xyz.cif.gz'
+        pdb_dir = os.getenv('PDB_DIR')
+        if not pdb_dir:
+            return
+        cif_4xyz = pdb_dir + '/structures/divided/mmCIF/xy/4xyz.cif.gz'
         self.assertEqual(gemmi.expand_pdb_code_to_path('4XYZ', 'M'), cif_4xyz)
         self.assertEqual(gemmi.expand_if_pdb_code('4XYZ'), cif_4xyz)
         self.assertEqual(gemmi.expand_if_pdb_code('4XYZ', filetype='P'),
-                         '/data/structures/divided/pdb/xy/pdb4xyz.ent.gz')
+                         pdb_dir + '/structures/divided/pdb/xy/pdb4xyz.ent.gz')
 
 if __name__ == '__main__':
     unittest.main()
