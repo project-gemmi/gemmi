a912e29c2b76da011d3143f275111f2226b3d909
Marcin Wojdyr <wojdyr@gmail.com>
2019-09-13 19:29:35 +0200

    WIP: alignment

---
+++ include/gemmi/seqalign.hpp
@@ -0,0 +1,155 @@
+// Code in this file is based on and derived from files ksw2_gg.c and ksw2.h
+// from https://github.com/lh3/ksw2, which is under the MIT license.
+// The original code, written by Heng Li, has more features and has more
+// efficient variants that use SSE instructions.
+
+#ifndef GEMMI_SEQALIGN_HPP_
+#define GEMMI_SEQALIGN_HPP_
+
+#include <cstdint>
+#include <algorithm> // for reverse
+#include <string>
+#include <vector>
+
+namespace gemmi {
+
+struct Alignment {
+  struct Item {
+    std::uint32_t value;
+    char op() const { return "MID"[value & 0xf]; }
+    std::uint32_t len() const { return value >> 4; }
+  };
+  int score;
+  std::vector<Item> cigar;
+
+  std::string cigar_str() const {
+    std::string s;
+    for (Item item : cigar) {
+      s += std::to_string(item.len());
+      s += item.op();
+    }
+    return s;
+  }
+
+  // In the backtrack matrix, value p[] has the following structure:
+  //   bit 0-2: which type gets the max - 0 for H, 1 for E, 2 for F
+  //   bit 3/0x08: 1 if a continuation on the E state
+  //   bit 4/0x10: 1 if a continuation on the F state
+  void backtrack_to_cigar(const std::uint8_t *p, int i, int j) {
+    i--;
+    int j0 = j--;
+    while (i >= 0 && j >= 0) {
+      // at the beginning of the loop, _state_ tells us which state to check
+      // if requesting the H state, find state one maximizes it.
+      int state = p[(size_t)i * j0 + j] & 7;
+      if (state == 0) { // match
+        push_cigar(0, 1);
+        --i;
+        --j;
+      } else if (state == 1) { // deletion
+        push_cigar(2, 1);
+        --i;
+      } else { // insertion
+        push_cigar(1, 1);
+        --j;
+      }
+    }
+    if (i >= 0)
+      push_cigar(2, i + 1); // first deletion
+    else if (j >= 0)
+      push_cigar(1, j + 1); // first insertion
+    std::reverse(cigar.begin(), cigar.end());
+  }
+
+private:
+  void push_cigar(std::uint32_t op, int len) {
+    if (cigar.empty() || op != (cigar.back().value & 0xf))
+      cigar.push_back({len<<4 | op});
+    else
+      cigar.back().value += len<<4;
+  }
+};
+
+
+Alignment align_sequences(int qlen, const std::uint8_t *query,
+                          int tlen, const std::uint8_t *target,
+                          std::int8_t m, const std::int8_t *mat,
+                          std::int8_t gapo, std::int8_t gape) {
+  // generate the query profile
+  std::int8_t *query_profile = new std::int8_t[qlen * m];
+  for (std::int32_t k = 0, i = 0; k < m; ++k)
+    for (std::int32_t j = 0; j < qlen; ++j)
+      query_profile[i++] = mat[k * m + query[j]];
+
+  struct eh_t { std::int32_t h, e; };
+  eh_t *eh = new eh_t[qlen + 1];
+  std::int32_t gapoe = gapo + gape;
+  // fill the first row
+  eh[0].h = 0;
+  eh[0].e = -gapoe - gapoe;
+  for (std::int32_t j = 1; j <= qlen; ++j) {
+    eh[j].h = -(gapoe + gape * (j - 1));
+    eh[j].e = -(gapoe + gapoe + gape * j);
+  }
+
+  // backtrack matrix; in each cell: f<<4|e<<2|h
+  std::uint8_t *z = new std::uint8_t[(size_t)qlen * tlen];
+  // DP loop
+  for (std::int32_t i = 0; i < tlen; ++i) {
+    std::uint8_t target_item = target[i];
+    std::int8_t *scores = &query_profile[target_item * qlen];
+    std::uint8_t *zi = &z[(size_t)i * qlen];
+    std::int32_t h1 = -(gapoe + gape * i);
+    std::int32_t f = -(gapoe + gapoe + gape * i);
+    for (std::int32_t j = 0; j < qlen; ++j) {
+      // At the beginning of the loop:
+      //  eh[j] = { H(i-1,j-1), E(i,j) }, f = F(i,j) and h1 = H(i,j-1)
+      // Cells are computed in the following order:
+      //   H(i,j)   = max{H(i-1,j-1) + S(i,j), E(i,j), F(i,j)}
+      //   E(i+1,j) = max{H(i,j)-gapo, E(i,j)} - gape
+      //   F(i,j+1) = max{H(i,j)-gapo, F(i,j)} - gape
+      eh_t *p = &eh[j];
+      std::int32_t h = p->h;
+      std::int32_t e = p->e;
+      p->h = h1;
+      h += scores[j];
+      std::uint8_t direction = 0;
+      if (h < e) {
+        direction = 1;
+        h = e;
+      }
+      if (h < f) {
+        direction = 2;
+        h = f;
+      }
+      h1 = h;
+      h -= gapoe;
+      e -= gape;
+      if (e > h)
+        direction |= 0x08;
+      else
+        e = h;
+      p->e = e;
+      f -= gape;
+      if (f > h)
+        direction |= 0x10;
+      else
+        f = h;
+      // z[i,j] keeps h for the current cell and e/f for the next cell
+      zi[j] = direction;
+    }
+    eh[qlen].h = h1;
+    eh[qlen].e = -0x40000000; // -infinity
+  }
+
+  Alignment result;
+  result.score = eh[qlen].h;
+  delete [] query_profile;
+  delete [] eh;
+  result.backtrack_to_cigar(z, tlen, qlen);
+  delete [] z;
+  return result;
+}
+
+} // namespace gemmi
+#endif
---
+++ src/seq.cpp
@@ -0,0 +1,175 @@
+// Copyright 2019 Global Phasing Ltd.
+//
+// This program compares sequence from SEQRES and from the model.
+
+#include <gemmi/model.hpp>
+#include <gemmi/gzread.hpp>
+#include <gemmi/polyheur.hpp>  // for add_entity_types, setup_entities
+#include <gemmi/seqalign.hpp>  // for align_sequences
+
+#define GEMMI_PROG seq
+#include "options.h"
+#include <stdio.h>
+#include <cstring>  // for strlen
+
+enum OptionIndex { Verbose=3, TextAlign };
+
+static const option::Descriptor Usage[] = {
+  { NoOp, 0, "", "", Arg::None,
+    "Usage:\n " EXE_NAME " [options] INPUT[...]"
+    "\nCompare sequence (SEQRES) with model from a PDB or mmCIF file."},
+  { Help, 0, "h", "help", Arg::None, "  -h, --help  \tPrint usage and exit." },
+  { Version, 0, "V", "version", Arg::None,
+    "  -V, --version  \tPrint version and exit." },
+  { Verbose, 0, "v", "verbose", Arg::None, "  --verbose  \tVerbose output." },
+  { TextAlign, 0, "", "text-align", Arg::None,
+    "  --text-align  \tAlign characters in two strings (for testing)." },
+  { 0, 0, 0, 0, 0, 0 }
+};
+
+
+static void print_text_alignment(const char* text1, const char* text2) {
+  int len1 = (int) std::strlen(text1);
+  int len2 = (int) std::strlen(text2);
+  std::vector<uint8_t> v1(len1);
+  for (int i = 0; i != len1; ++i)
+    v1[i] = text1[i] >= 32 && text1[i] < 127 ? text1[i] - 32 : 0;
+  std::vector<uint8_t> v2(len2);
+  for (int i = 0; i != len2; ++i)
+    v2[i] = text2[i] >= 32 && text2[i] < 127 ? text2[i] - 32 : 0;
+  int m = 127 - 32;
+  std::vector<int8_t> score_matrix(m * m, -1);
+  for (int i = 0; i < m; ++i)
+    score_matrix[i * m + i] = 1;
+  gemmi::Alignment result = gemmi::align_sequences(
+      len1, v1.data(),
+      len2, v2.data(),
+      m, score_matrix.data(),
+      /*gapo*/1, /*gape*/1);
+  printf("Score: %d   CIGAR: %s\n", result.score, result.cigar_str().c_str());
+  size_t pos1 = 0;
+  size_t pos2 = 0;
+  std::string match, out1, out2;
+  for (gemmi::Alignment::Item item : result.cigar) {
+    char op = item.op();
+    for (uint32_t i = 0; i < item.len(); ++i) {
+      if (op == 'I') {
+        match += 'I';
+        out1 += text1[pos1++];
+        out2 += '-';
+      } else if (op == 'D') {
+        match += 'D';
+        out1 += '-';
+        out2 += text2[pos2++];
+      } else /* if (op == 'M') */ {
+        match += v1[pos1] == v2[pos2] ? '=' : 'X';
+        out1 += text1[pos1++];
+        out2 += text2[pos2++];
+      }
+    }
+  }
+  printf("%s\n%s\n%s\n", match.c_str(), out1.c_str(), out2.c_str());
+}
+
+
+static void print_sequence_alignment(const gemmi::Structure& st, bool verbose) {
+  if (st.models.size() > 1)
+    std::fprintf(stderr, "Warning: using only the first model out of %zu.\n",
+                 st.models.size());
+  const gemmi::Model& model = st.models.at(0);
+  for (const gemmi::Chain& chain : model.chains) {
+    gemmi::ConstResidueSpan polymer = chain.get_polymer();
+    if (!polymer)
+      continue;
+    const gemmi::Entity* ent = st.get_entity_of(polymer);
+    if (!ent)
+      gemmi::fail("Full sequence (SEQRES) not found for chain " + chain.name);
+    std::map<std::string, uint8_t> encoding;
+    for (const gemmi::Residue& res : polymer)
+      encoding.emplace(res.name, 0);
+    for (const std::string& mon_list : ent->full_sequence)
+      encoding.emplace(gemmi::Entity::first_mon(mon_list), 0);
+    std::vector<std::string> all_monomers;
+    size_t n_mon = encoding.size();
+    all_monomers.reserve(n_mon);
+    for (auto& item : encoding) {
+      item.second = (uint8_t) all_monomers.size();
+      all_monomers.push_back(item.first);
+    }
+    std::vector<uint8_t> model_seq;
+    model_seq.reserve(polymer.size());
+    // TODO include gaps based on distances
+    for (const gemmi::Residue& res : polymer)
+      model_seq.push_back(encoding.at(res.name));
+    std::vector<uint8_t> full_seq;
+    full_seq.reserve(ent->full_sequence.size());
+    for (const std::string& mon_list : ent->full_sequence)
+      full_seq.push_back(encoding.at(gemmi::Entity::first_mon(mon_list)));
+    std::vector<int8_t> score_matrix(n_mon * n_mon, -2);
+    for (size_t i = 0; i != n_mon; ++i)
+      score_matrix[i * n_mon + i] = 4;
+    gemmi::Alignment result = gemmi::align_sequences(
+        full_seq.size(), full_seq.data(),
+        model_seq.size(), model_seq.data(),
+        n_mon, score_matrix.data(),
+        /*gapo*/4, /*gape*/2);
+    printf("%s chain %s CIGAR: %s\n",
+           st.name.c_str(), chain.name.c_str(), result.cigar_str().c_str());
+    if (verbose) {
+      size_t model_pos = 0;
+      size_t full_pos = 0;
+      for (gemmi::Alignment::Item item : result.cigar) {
+        char op = item.op();
+        for (uint32_t i = 0; i < item.len(); ++i) {
+          bool ok = op == 'I' ||
+                    (op == 'M' && full_seq[full_pos] == model_seq[model_pos]);
+          printf("  %s ", chain.name.c_str());
+          if (op == 'I' || op == 'M') {
+            uint8_t enc = full_seq.at(full_pos++);
+            printf("%4zu %-3s -", full_pos, all_monomers.at(enc).c_str());
+          } else {
+            printf("         -");
+          }
+          if (op == 'D' || op == 'M') {
+            const gemmi::Residue& res = polymer.at(model_pos++);
+            printf(" %-3s %4d%c",
+                   res.name.c_str(), (int) res.seqid.num, res.seqid.icode);
+          }
+          if (!ok)
+            printf("    <-- BAD");
+          printf("\n");
+        }
+      }
+    }
+  }
+}
+
+int GEMMI_MAIN(int argc, char **argv) {
+  OptParser p(EXE_NAME);
+  p.simple_parse(argc, argv, Usage);
+  if (p.options[TextAlign]) {
+    p.require_positional_args(2);
+    print_text_alignment(p.nonOption(0), p.nonOption(1));
+    return 0;
+  }
+  p.require_input_files_as_args();
+  bool verbose = p.options[Verbose];
+  try {
+    for (int i = 0; i < p.nonOptionsCount(); ++i) {
+      std::string input = p.coordinate_input_file(i);
+      if (i > 0)
+        std::printf("\n");
+      if (verbose || p.nonOptionsCount() > 1)
+        std::printf("File: %s\n", input.c_str());
+      gemmi::Structure st = gemmi::read_structure_gz(input);
+      gemmi::setup_entities(st);
+      print_sequence_alignment(st, verbose);
+    }
+  } catch (std::runtime_error& e) {
+    std::fprintf(stderr, "ERROR: %s\n", e.what());
+    return 1;
+  }
+  return 0;
+}
+
+// vim:sw=2:ts=2:et:path^=../include,../third_party
