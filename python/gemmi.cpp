48785a0c988630c8415e84cb7e439e965acadbf6
Marcin Wojdyr <wojdyr@gmail.com>
2019-10-22 15:30:09 +0200

    python bindings: moving code between files

--- python/gemmi.cpp
+++ python/gemmi.cpp
@@ -1,11 +1,12 @@
 // Copyright 2017 Global Phasing Ltd.
 
-#include <sstream>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include "gemmi/version.hpp"
 #include "gemmi/dirwalk.hpp"
 #include "gemmi/fileutil.hpp"  // for expand_if_pdb_code
+#include "gemmi/elem.hpp"
+#include "gemmi/smcif.hpp"
 
 namespace py = pybind11;
 
@@ -17,8 +18,65 @@ void add_mol(py::module& m); // mol.cpp
 void add_cif(py::module& cif); // cif.cpp
 void add_read_structure(py::module& m); // read.cpp
 void add_cif_read(py::module& cif); // read.cpp
-void add_smcif(py::module& cif); // smallmol.cpp
-void add_monlib(py::module& cif); // smallmol.cpp
+void add_monlib(py::module& cif); // monlib.cpp
+
+void add_misc(py::module& m) {
+  using gemmi::Element;
+  using gemmi::AtomicStructure;
+  py::class_<Element>(m, "Element")
+    .def(py::init<const std::string &>())
+    .def(py::init<int>())
+    .def("__eq__",
+         [](const Element &a, const Element &b) { return a.elem == b.elem; },
+         py::is_operator())
+#if PY_MAJOR_VERSION < 3  // in Py3 != is inferred from ==
+    .def("__ne__",
+         [](const Element &a, const Element &b) { return a.elem != b.elem; },
+         py::is_operator())
+#endif
+    .def_property_readonly("name", &Element::name)
+    .def_property_readonly("weight", &Element::weight)
+    .def_property_readonly("covalent_r", &Element::covalent_r)
+    .def_property_readonly("atomic_number", &Element::atomic_number)
+    .def("__repr__", [](const Element& self) {
+        return "<gemmi.Element: " + std::string(self.name()) + ">";
+    });
+
+  py::class_<AtomicStructure> atomic_structure(m, "AtomicStructure");
+  py::class_<AtomicStructure::Site>(atomic_structure, "Site")
+    .def_readonly("label", &AtomicStructure::Site::label)
+    .def_readonly("type_symbol", &AtomicStructure::Site::type_symbol)
+    .def_readonly("fract", &AtomicStructure::Site::fract)
+    .def("__repr__", [](const AtomicStructure::Site& self) {
+        return "<gemmi.AtomicStructure.Site " + self.label + ">";
+    });
+
+  atomic_structure
+    .def(py::init<>())
+    .def_readwrite("name", &AtomicStructure::name)
+    .def_readwrite("cell", &AtomicStructure::cell)
+    .def_readonly("spacegroup_hm", &AtomicStructure::spacegroup_hm)
+    .def_readonly("sites", &AtomicStructure::sites)
+    .def("get_all_unit_cell_sites", &AtomicStructure::get_all_unit_cell_sites)
+    .def("__repr__", [](const AtomicStructure& self) {
+        return "<gemmi.AtomicStructure: " + std::string(self.name) + ">";
+    });
+
+  py::class_<gemmi::CifWalk>(m, "CifWalk")
+    .def(py::init<const char*>())
+    .def("__iter__", [](gemmi::CifWalk& self) {
+        return py::make_iterator(self);
+    }, py::keep_alive<0, 1>());
+  py::class_<gemmi::CoorFileWalk>(m, "CoorFileWalk")
+    .def(py::init<const char*>())
+    .def("__iter__", [](gemmi::CoorFileWalk& self) {
+        return py::make_iterator(self);
+    }, py::keep_alive<0, 1>());
+  m.def("is_pdb_code", &gemmi::is_pdb_code);
+  m.def("expand_pdb_code_to_path", &gemmi::expand_pdb_code_to_path);
+  m.def("expand_if_pdb_code", &gemmi::expand_if_pdb_code,
+        py::arg("code"), py::arg("filetype")='M');
+}
 
 PYBIND11_MODULE(gemmi, mg) {
   mg.doc() = "General MacroMolecular I/O";
@@ -27,26 +85,11 @@ PYBIND11_MODULE(gemmi, mg) {
   add_grid(mg);
   add_unitcell(mg);
   add_hkl(mg);
+  add_misc(mg);
   add_mol(mg);
-  add_smcif(mg);
   add_monlib(mg);
   add_read_structure(mg);
   py::module cif = mg.def_submodule("cif", "CIF file format");
   add_cif(cif);
   add_cif_read(cif);
-
-  py::class_<gemmi::CifWalk>(mg, "CifWalk")
-    .def(py::init<const char*>())
-    .def("__iter__", [](gemmi::CifWalk& self) {
-        return py::make_iterator(self);
-    }, py::keep_alive<0, 1>());
-  py::class_<gemmi::CoorFileWalk>(mg, "CoorFileWalk")
-    .def(py::init<const char*>())
-    .def("__iter__", [](gemmi::CoorFileWalk& self) {
-        return py::make_iterator(self);
-    }, py::keep_alive<0, 1>());
-  mg.def("is_pdb_code", &gemmi::is_pdb_code);
-  mg.def("expand_pdb_code_to_path", &gemmi::expand_pdb_code_to_path);
-  mg.def("expand_if_pdb_code", &gemmi::expand_if_pdb_code,
-         py::arg("code"), py::arg("filetype")='M');
 }
--- python/monlib.cpp
+++ python/monlib.cpp
@@ -1,7 +1,5 @@
 // Copyright 2018 Global Phasing Ltd.
 
-#include "gemmi/elem.hpp"
-#include "gemmi/smcif.hpp"
 #include "gemmi/chemcomp.hpp"
 #include "gemmi/monlib.hpp"
 #include "gemmi/gzread.hpp" // read_cif_gz
@@ -24,54 +22,13 @@ PYBIND11_MAKE_OPAQUE(monomers_type)
 PYBIND11_MAKE_OPAQUE(links_type)
 PYBIND11_MAKE_OPAQUE(modifications_type)
 
-void add_smcif(py::module& m) {
+void add_monlib(py::module& m) {
   py::bind_vector<std::vector<Restraints::Bond>>(m, "RestraintsBonds");
   py::bind_vector<std::vector<ChemComp::Atom>>(m, "ChemCompAtoms");
   py::bind_map<monomers_type>(m, "ChemCompMap");
   py::bind_map<links_type>(m, "ChemLinkMap");
   py::bind_map<modifications_type>(m, "ChemModMap");
 
-  py::class_<Element>(m, "Element")
-    .def(py::init<const std::string &>())
-    .def(py::init<int>())
-    .def("__eq__",
-         [](const Element &a, const Element &b) { return a.elem == b.elem; },
-         py::is_operator())
-#if PY_MAJOR_VERSION < 3  // in Py3 != is inferred from ==
-    .def("__ne__",
-         [](const Element &a, const Element &b) { return a.elem != b.elem; },
-         py::is_operator())
-#endif
-    .def_property_readonly("name", &Element::name)
-    .def_property_readonly("weight", &Element::weight)
-    .def_property_readonly("covalent_r", &Element::covalent_r)
-    .def_property_readonly("atomic_number", &Element::atomic_number)
-    .def("__repr__", [](const Element& self) {
-        return "<gemmi.Element: " + std::string(self.name()) + ">";
-    });
-
-  py::class_<AtomicStructure> atomic_structure(m, "AtomicStructure");
-  py::class_<AtomicStructure::Site>(atomic_structure, "Site")
-    .def_readonly("label", &AtomicStructure::Site::label)
-    .def_readonly("type_symbol", &AtomicStructure::Site::type_symbol)
-    .def_readonly("fract", &AtomicStructure::Site::fract)
-    .def("__repr__", [](const AtomicStructure::Site& self) {
-        return "<gemmi.AtomicStructure.Site " + self.label + ">";
-    });
-
-  atomic_structure
-    .def(py::init<>())
-    .def_readwrite("name", &AtomicStructure::name)
-    .def_readwrite("cell", &AtomicStructure::cell)
-    .def_readonly("spacegroup_hm", &AtomicStructure::spacegroup_hm)
-    .def_readonly("sites", &AtomicStructure::sites)
-    .def("get_all_unit_cell_sites", &AtomicStructure::get_all_unit_cell_sites)
-    .def("__repr__", [](const AtomicStructure& self) {
-        return "<gemmi.AtomicStructure: " + std::string(self.name) + ">";
-    });
-}
-
-void add_monlib(py::module& m) {
   py::enum_<BondType>(m, "BondType")
     .value("Unspec", BondType::Unspec)
     .value("Single", BondType::Single)
