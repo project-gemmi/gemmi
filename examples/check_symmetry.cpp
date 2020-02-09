1141ac8f53e19f16226e9bd9542821f6e56e08fa
Marcin Wojdyr <wojdyr@gmail.com>
2020-02-09 23:45:56 +0100

    Example program that reports discrepancies in symmetry in PDB files
    It compares symmetry in CRYST1 and in REMARK 290.
    When run on the local copy of the PDB database it outputs:
    
    no remark 290: structures/divided/pdb/a6/pdb2a6u.ent.gz
    structures/divided/pdb/af/pdb4afl.ent.gz
    CRYST1: C 2 2 2 -> C 2 2 2
    REMARK 290: 8 symops -> C 2 2 21
    [progress: 10000 files]
    structures/divided/pdb/bk/pdb2bk5.ent.gz
    CRYST1: C 2 2 2 -> C 2 2 2
    REMARK 290: 8 symops -> C 2 2 21
    no remark 290: structures/divided/pdb/c4/pdb1c4s.ent.gz
    no remark 290: structures/divided/pdb/c4/pdb2c4s.ent.gz
    ...

---
+++ examples/check_symmetry.cpp
@@ -0,0 +1,46 @@
+// This program reads PDB files and reports discrepancies between
+// symmetry from CRYST1 and symmetry from REMARK 290.
+
+#include <cstdio>
+#include <gemmi/dirwalk.hpp> // for PdbWalk
+#include <gemmi/gz.hpp>      // for MaybeGzipped
+#include <gemmi/pdb.hpp>     // for read_pdb
+#include <gemmi/remarks.hpp> // for read_remark_290
+
+void check_remark290(const std::string& path) {
+  using namespace gemmi;
+  Structure st = read_pdb(MaybeGzipped(path));
+  std::vector<Op> ops = read_remark_290(st.raw_remarks);
+  const SpaceGroup* sg = find_spacegroup_by_ops(split_centering_vectors(ops));
+  if (sg) {
+    const SpaceGroup* cryst1_sg = st.find_spacegroup();
+    if (sg != cryst1_sg) {
+      printf("%s\n", path.c_str());
+      printf("CRYST1: %s -> %s\n", st.spacegroup_hm.c_str(),
+             cryst1_sg ? cryst1_sg->xhm().c_str() : "n/a");
+      printf("REMARK 290: %zu symops -> %s\n", ops.size(), sg->xhm().c_str());
+    }
+  } else {
+    if (st.cell.is_crystal())
+      printf("no remark 290: %s\n", path.c_str());
+  }
+}
+
+int main(int argc, char* argv[]) {
+  int counter = 0;
+  try {
+    for (int pos = 1; pos < argc; ++pos) {
+      for (const std::string& path : gemmi::PdbWalk(argv[pos])) {
+        check_remark290(path);
+        if (++counter % 10000 == 0) {
+          std::printf("[progress: %d files]\n", counter);
+          std::fflush(stdout);
+        }
+      }
+    }
+  } catch (std::runtime_error& err) {
+    std::fprintf(stderr, "Error: %s\n", err.what());
+    return 1;
+  }
+  return 0;
+}
