638ee97c355f3285abc21d754c8d811a1fc59bb3
Marcin Wojdyr <wojdyr@gmail.com>
2020-08-20 12:51:20 +0200

    aligned_alloc - disable on Mac again

--- include/gemmi/third_party/pocketfft_hdronly.h
+++ include/gemmi/third_party/pocketfft_hdronly.h
@@ -157,6 +157,7 @@ template<typename T> class arr
     static void dealloc(T *ptr)
       { free(ptr); }
 #elif __cplusplus >= 201703L && !defined(_LIBCPP_HAS_NO_ALIGNED_ALLOCATION) && \
+  !defined(__APPLE__) && \
   (!defined(__MINGW32__) || defined(_GLIBCXX_HAVE_ALIGNED_ALLOC))
     static T *ralloc(size_t num)
       {
