56e349c10dd29d54b814bad1a1db3796bd8f056a
Marcin Wojdyr <wojdyr@gmail.com>
2021-04-06 12:17:27 +0200

    upstream-check.py: update to python3

--- tools/upstream-check.py
+++ tools/upstream-check.py
@@ -1,36 +1,38 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 # Check for new versions of third-party libraries
 
 from __future__ import print_function
 import datetime
 import json
-import urllib
+from urllib.request import urlopen
 
 TAGGED_REPOS = {
     'pybind/pybind11': 'v2.6.1',
     'taocpp/PEGTL': '2.4.0',
     'cxong/tinydir': '1.2.4',
-    'madler/zlib': 'v1.2.11',
+    #'madler/zlib': 'v1.2.11',
 }
 
 NOT_TAGGED_REPOS = {
     'chadaustin/sajson': 'include/sajson.h',
     'sgorsten/linalg': 'linalg.h',
     'nothings/stb': 'stb_sprintf.h',
+    'fastfloat/fast_float': 'include/fast_float',
 }
+
+def load_json(url):
+    with urlopen(url) as response:
+        return json.load(response)
 
 def check_tags():
     for repo, version in TAGGED_REPOS.items():
         url = 'https://api.github.com/repos/%s/releases/latest' % repo
-        response = urllib.urlopen(url)
-        data = json.load(response)
+        data = load_json(url)
         if 'tag_name' in data:
             latest_tag = data['tag_name']
         else:
-            url = 'https://api.github.com/repos/%s/tags' % repo
-            response = urllib.urlopen(url)
-            data = json.load(response)
+            data = load_json('https://api.github.com/repos/%s/tags' % repo)
             latest_tag = data[0]['name']
         mark = ('   !!!' if version != latest_tag else '')
         print('%-18s %10s %10s%s' % (repo, version, latest_tag, mark))
@@ -39,9 +41,8 @@ def check_recent_commits():
     since = datetime.datetime.now() - datetime.timedelta(days=30)
     for repo, filename in NOT_TAGGED_REPOS.items():
         url = 'https://api.github.com/repos/%s/commits?path=%s&since=%s' % (
-              repo, filename, since)
-        response = urllib.urlopen(url)
-        data = json.load(response)
+              repo, filename, since.isoformat())
+        data = load_json(url)
         info = '-'
         if data:
             info = data[0]['commit']['committer']['date'][:10]
