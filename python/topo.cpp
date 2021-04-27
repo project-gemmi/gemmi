e7ccd5324dd127599ae74623ab12a0c87b991ab6
keitaroyam <keitaroyam@users.noreply.github.com>
2021-04-27 11:30:52 +0100

    added missing Topo python bindings

--- python/topo.cpp
+++ python/topo.cpp
@@ -16,6 +16,10 @@ PYBIND11_MAKE_OPAQUE(std::vector<Topo::Torsion>)
 PYBIND11_MAKE_OPAQUE(std::vector<Topo::Chirality>)
 PYBIND11_MAKE_OPAQUE(std::vector<Topo::Plane>)
 PYBIND11_MAKE_OPAQUE(std::vector<Topo::ExtraLink>)
+PYBIND11_MAKE_OPAQUE(std::vector<Topo::ChainInfo>)
+PYBIND11_MAKE_OPAQUE(std::vector<Topo::ResInfo::Prev>)
+PYBIND11_MAKE_OPAQUE(std::vector<Topo::ResInfo>)
+PYBIND11_MAKE_OPAQUE(std::vector<Topo::Force>)
 
 void add_topo(py::module& m) {
   py::class_<Topo> topo(m, "Topo");
@@ -56,6 +60,45 @@ void add_topo(py::module& m) {
     .def_readonly("atoms", &Topo::Plane::atoms)
     .def("has", &Topo::Plane::has)
     ;
+  py::enum_<Topo::Provenance>(m, "Provenance")
+    .value("None", Topo::Provenance::None)
+    .value("PrevLink", Topo::Provenance::PrevLink)
+    .value("Monomer", Topo::Provenance::Monomer)
+    .value("NextLink", Topo::Provenance::NextLink)
+    .value("ExtraLink", Topo::Provenance::ExtraLink)
+    ;
+  py::enum_<Topo::RKind>(m, "RKind")
+    .value("Bond", Topo::RKind::Bond)
+    .value("Angle", Topo::RKind::Angle)
+    .value("Torsion", Topo::RKind::Torsion)
+    .value("Chirality", Topo::RKind::Chirality)
+    .value("Plane", Topo::RKind::Plane)
+    ;
+  py::class_<Topo::Force>(topo, "Force")
+    .def_readonly("provenance", &Topo::Force::provenance)
+    .def_readonly("rkind", &Topo::Force::rkind)
+    .def_readonly("index", &Topo::Force::index)
+    ;
+  py::class_<Topo::ResInfo> resinfo(topo, "ResInfo");
+  py::class_<Topo::ResInfo::Prev>(resinfo, "Prev")
+    .def_readonly("link", &Topo::ResInfo::Prev::link)
+    .def_readonly("idx", &Topo::ResInfo::Prev::idx)
+    .def("get", (Topo::ResInfo* (Topo::ResInfo::Prev::*)(Topo::ResInfo*)const) &Topo::ResInfo::Prev::get, py::keep_alive<0, 1>())
+    ;
+  resinfo
+    .def_readonly("res", &Topo::ResInfo::res)
+    .def_readonly("prev", &Topo::ResInfo::prev)
+    .def_readonly("mods", &Topo::ResInfo::mods)
+    .def_readonly("chemcomp", &Topo::ResInfo::chemcomp)
+    .def_readonly("forces", &Topo::ResInfo::forces)
+    ;
+  py::class_<Topo::ChainInfo>(topo, "ChainInfo")
+    .def_readonly("name", &Topo::ChainInfo::name)
+    .def_readonly("entity_id", &Topo::ChainInfo::entity_id)
+    .def_readonly("polymer", &Topo::ChainInfo::polymer)
+    .def_readonly("polymer_type", &Topo::ChainInfo::polymer_type)
+    .def_readonly("res_infos", &Topo::ChainInfo::res_infos)
+    ;
   py::class_<Topo::ExtraLink>(topo, "ExtraLink")
     .def_readonly("res1", &Topo::ExtraLink::res1)
     .def_readonly("res2", &Topo::ExtraLink::res2)
@@ -69,6 +112,10 @@ void add_topo(py::module& m) {
   py::bind_vector<std::vector<Topo::Torsion>>(m, "TopoTorsions");
   py::bind_vector<std::vector<Topo::Chirality>>(m, "TopoChirs");
   py::bind_vector<std::vector<Topo::Plane>>(m, "TopoPlanes");
+  py::bind_vector<std::vector<Topo::ChainInfo>>(m, "TopoChainInfos");
+  py::bind_vector<std::vector<Topo::Force>>(m, "TopoForces");
+  py::bind_vector<std::vector<Topo::ResInfo::Prev>>(m, "TopoResInfoPrevs");
+  py::bind_vector<std::vector<Topo::ResInfo>>(m, "TopoResInfos");
   py::bind_vector<std::vector<Topo::ExtraLink>>(m, "TopoExtraLinks");
 
   topo
@@ -81,6 +128,7 @@ void add_topo(py::module& m) {
     .def_readonly("chirs", &Topo::chirs)
     .def_readonly("planes", &Topo::planes)
     .def_readonly("extras", &Topo::extras)
+    .def_readonly("chain_infos", &Topo::chain_infos)
     ;
 
   m.def("prepare_topology", &prepare_topology,
