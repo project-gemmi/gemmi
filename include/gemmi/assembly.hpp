e0a0f5911d95687e35e3986eb0bad1e998c466d4
Marcin Wojdyr <wojdyr@gmail.com>
2021-06-05 20:41:44 +0200

    fix make_short_name(): '\0' was used after A-Za-z0-9

--- include/gemmi/assembly.hpp
+++ include/gemmi/assembly.hpp
@@ -35,8 +35,13 @@ struct ChainNameGenerator {
   }
 
   std::string make_short_name(const std::string& preferred) {
-    static const char symbols[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                                  "abcdefghijklmnopqrstuvwxyz0123456789";
+    static const char symbols[] = {
+      'A','B','C','D','E','F','G','H','I','J','K','L','M',
+      'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+      'a','b','c','d','e','f','g','h','i','j','k','l','m',
+      'n','o','p','q','r','s','t','u','v','w','x','y','z',
+      '0','1','2','3','4','5','6','7','8','9'
+    };
     if (!has(preferred))
       return added(preferred);
     std::string name(1, 'A');
