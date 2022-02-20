8f8e70619e57c62d0cc4ade3399407b4e67db503
Marcin Wojdyr <wojdyr@gmail.com>
2022-02-20 18:48:45 +0100

    scripts for comparing masks

--- examples/maskcheck.py
+++ examples/maskcheck.py
@@ -6,11 +6,12 @@
 #   cctbx.python -m mmtbx.command_line.mask file.pdb
 #   maskcheck.py mask.ccp4 file.pdb
 
+import os
 import sys
 import numpy
 import gemmi
 
-def maskcheck(mask_path, coor_path):
+def maskcheck(mask_path, coor_path, output_diff_map=None):
     # read mask
     mask = gemmi.read_ccp4_mask(mask_path)
     mask.setup()
@@ -20,20 +21,29 @@ def maskcheck(mask_path, coor_path):
     st = gemmi.read_structure(coor_path)
 
     # check if unit cell and symmetry are the same
-    if grid.unit_cell.parameters != st.cell.parameters:
-        print('Cell parameters differ:',
-              grid.unit_cell.parameters, 'vs', st.cell.parameters)
+    if not grid.unit_cell.approx(st.cell, epsilon=1e-4):
+        print('Cell parameters differ. PDB:', st.cell.parameters)
+        print('                       Mask:', grid.unit_cell.parameters)
     sg = st.find_spacegroup()
     if grid.spacegroup != sg:
         print('Space groups differ:', grid.spacegroup.xhm(), 'vs', sg.xhm())
 
     # prepare a mask with the same size
-    masker = gemmi.SolventMasker(gemmi.AtomicRadiiSet.Cctbx)
+    if 'REFMAC' in os.environ:
+        print('Generating REFMAC-compatible mask ...')
+        masker = gemmi.SolventMasker(gemmi.AtomicRadiiSet.Refmac)
+        masker.island_min_volume = 8 * 2.8**3
+    else:
+        print('Generating CCTBX-compatible mask ...')
+        masker = gemmi.SolventMasker(gemmi.AtomicRadiiSet.Cctbx)
     grid2 = gemmi.Int8Grid()
     grid2.copy_metadata_from(grid)
     masker.put_mask_on_int8_grid(grid2, st[0])
 
     compare_mask_arrays(grid, grid2)
+    #print_nearby_atoms(st, grid, grid2)
+    if output_diff_map:
+        write_diff_map(grid, grid2, output_diff_map)
 
 
 def compare_mask_arrays(grid1, grid2):
@@ -42,15 +52,33 @@ def compare_mask_arrays(grid1, grid2):
     if arr1.shape != arr2.shape:
         sys.exit('Different grid sizes %s and %s. Exiting.' %
                  (arr1.shape, arr2.shape))
-    else:
-        print('Size: %d x %d x %d,' % arr1.shape, 'total', arr1.size, 'points')
+    print('Size: %d x %d x %d,' % arr1.shape, 'total', arr1.size, 'points')
     t = 2 * (arr1 != 0) + (arr2 != 0)
+    print('File-Gemmi Count Fraction')
     for (a, b) in [(0, 0), (1, 1), (0, 1), (1, 0)]:
         n = numpy.count_nonzero(t == 2*a+b)
         print('%d-%d %12d %6.2f%%' % (a, b, n, 100.*n/arr1.size))
 
+# Print nearby atom for each differing point
+def print_nearby_atoms(st, grid1, grid2):
+    ns = gemmi.NeighborSearch(st[0], st.cell, 4).populate()
+    for (p1, p2) in zip(grid1, grid2):
+        if p1.value != p2.value:
+            pos = grid2.point_to_position(p2)
+            mark = ns.find_nearest_atom(pos)
+            cra = mark.to_cra(st[0])
+            print('%d-%d near %s' % (p1.value, p2.value, cra))
+
+def write_diff_map(grid1, grid2, output_diff_map):
+    arr_out = numpy.array(grid1, copy=False) - numpy.array(grid2, copy=False)
+    map_out = gemmi.Ccp4Map()
+    map_out.grid = gemmi.FloatGrid(arr_out.astype(dtype=numpy.float32))
+    map_out.grid.copy_metadata_from(grid1)
+    map_out.update_ccp4_header()
+    map_out.write_ccp4_map(output_diff_map)
+
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        sys.exit("Usage: maskcheck.py COORDINATE_FILE MAP_FILE")
-    maskcheck(sys.argv[1], sys.argv[2])
+    if len(sys.argv) not in (3, 4):
+        sys.exit("Usage: maskcheck.py MAP_FILE COORDINATE_FILE [OUTPUT_MAP]")
+    maskcheck(*sys.argv[1:])
---
+++ tools/compare_mask.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Compares bulk solvent masks from Refmac and Gemmi.
+# Requires PDB_DIR set to a local copy of the PDB archive,
+# and PDB_SF_DIR set to a local copy of structure_factors dir.
+# Example usage: ./compare_mask.sh 1keb
+
+set -eu
+REFMAC=${REFMAC-refmac5}
+LOCAL_COPY="$PDB_DIR/structures/divided"
+TEMPDIR=${TMPDIR-/tmp}/gemmi
+
+cd `dirname $0`/..
+BUILD_DIR="$(pwd)"
+[ -e build ] && BUILD_DIR="$(pwd)/build"
+PYTHON=`grep PYTHON_EXECUTABLE:FILEPATH= $BUILD_DIR/CMakeCache.txt | cut -d= -f2`
+export PYTHONPATH=$BUILD_DIR
+export PATH="$BUILD_DIR:$PATH"
+
+code=${1,,}
+pdb_tail="pdb/${code:1:2}/pdb${code}.ent.gz"
+uncompressed_pdb="$TEMPDIR/$code.pdb"
+mkdir -p "$TEMPDIR"
+echo "create $uncompressed_pdb"
+zcat "$LOCAL_COPY/$pdb_tail" > "$uncompressed_pdb"
+
+mtz="$TEMPDIR/$code.mtz"
+echo "create $mtz"
+sf_tail="${code:1:2}/r${code}sf.ent.gz"
+gemmi cif2mtz $PDB_SF_DIR/$sf_tail $mtz
+
+refmac_mask=$TEMPDIR/out.msk
+
+extra_refmac_options=
+#extra_refmac_options="solvent process islands noremove"
+export CCP_SUPPRESS_HTML=1
+refmac_log=$TEMPDIR/out.log
+echo "running $REFMAC, output redirected to $refmac_log"
+$REFMAC XYZIN $uncompressed_pdb HKLIN $mtz XYZOUT $TEMPDIR/out.pdb \
+        HKLOUT $TEMPDIR/out.mtz MSKOUT $refmac_mask >$refmac_log <<eof
+MAKE HYDRogens No
+REFI TYPE RIGID
+RIGIdbody NCYC 0
+$extra_refmac_options
+eof
+
+echo "Refmac mask should be in $refmac_mask, running:"
+echo "examples/maskcheck.py $refmac_mask $uncompressed_pdb"
+export REFMAC
+$PYTHON examples/maskcheck.py $refmac_mask $uncompressed_pdb
