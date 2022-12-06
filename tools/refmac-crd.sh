21b70c655c9d223658e2e479829df60dc1875c98
Marcin Wojdyr <wojdyr@gmail.com>
2022-12-06 22:12:01 +0100

    tweak tools/refmac-crd.sh

--- tools/refmac-crd.sh
+++ tools/refmac-crd.sh
@@ -2,6 +2,8 @@
 # Script that runs Refmac to generate intermediate files.
 # Arguments: input.pdb output.crd [libin.cif]
 # Output is concatenation of crd and rst files.
+# Alternatively, it can run 0 cycles of Refmac starting from crd.
+# Arguments: input.crd input.mtz output_basename
 
 set -eu
 
@@ -11,7 +13,11 @@ export CCP4=$HOME/ccp4/install
 export CLIB=$CCP4/lib
 export CLIBD=$CCP4/lib/data
 export CINCL=$CCP4/include
+export CCP_SUPPRESS_HTML=1
 REFMAC=$CCP4/bin/refmac5
+
+if [[ "$2" =~ \.crd$ ]]; then
+#### Preparing crd (pdb -> crd) ####
 
 export CCP4_SCR=${TMPDIR-/tmp}/refmac_crd
 mkdir -p $CCP4_SCR
@@ -22,7 +28,6 @@ if (( $# > 2 )); then
     libstr="libin $3"
 fi
 
-export CCP_SUPPRESS_HTML=1
 $REFMAC xyzin "$1" xyzout $CCP4_SCR/refmac5_.pdb $libstr << EOF
 ${REFMAC_EXTRA_RECORDS:-}
 make exit yes
@@ -32,3 +37,21 @@ EOF
 #make hydrogen no
 
 cat $CCP4_SCR/refmac5_temp1.*_new.crd $CCP4_SCR/refmac5_temp1.*_new.rst > "$2"
+echo "CRD file: $2"
+
+elif [[ "$2" =~ \.mtz$ ]]; then
+#### Using crd and mtz files in Refmac ####
+
+OUT=$3
+#labin FP=F SIGFP=SIGF FREE=FreeR_flag
+$REFMAC xyzin $1 hklin $2 xyzout $OUT.pdb hklout $OUT.mtz << EOF
+make cr prepared
+refinement type restrained
+ncycle 0
+end
+EOF
+echo "Refmac refinement output file: $OUT.*"
+
+else
+    echo "Unexpected arguments"
+fi
