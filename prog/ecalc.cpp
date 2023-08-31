fdfbde94748de8c733421d38766031baf19ba7ec
Marcin Wojdyr <wojdyr@gmail.com>
2023-08-31 23:22:53 +0200

    WIP: gemmi-ecalc

--- prog/ecalc.cpp
+++ prog/ecalc.cpp
@@ -2,6 +2,7 @@
 
 #include <cstdio>   // for printf, fprintf
 #include <cstdlib>  // for atof
+#include "gemmi/binner.hpp"
 #include "gemmi/mtz.hpp"
 
 #define GEMMI_PROG ecalc
@@ -54,8 +55,8 @@ int GEMMI_MAIN(int argc, char **argv) {
     mtz.read_file_gz(input);
     if (mtz.spacegroup == nullptr)
       gemmi::fail("Unknown space group of the input data: ", input);
-    const Mtz::Column& fcol = mtz.get_column_with_label(f_label);
-    if (use_sigma && !fcol.get_next_column_if_type('Q'))
+    const Mtz::Column* fcol = &mtz.get_column_with_label(f_label);
+    if (use_sigma && !fcol->get_next_column_if_type('Q'))
         gemmi::fail("Column ", f_label, " not followed by sigma column. Use --no-sigma.\n");
     int e_idx = -1;
     if (Mtz::Column* e = mtz.column_with_label(e_label)) {
@@ -69,21 +70,48 @@ int GEMMI_MAIN(int argc, char **argv) {
       std::fprintf(stderr, "%s column %s ...\n",
                    (e_idx >= 0 ? "Replacing existing" : "Adding"), e_label);
     std::vector<std::string> trailing_cols(use_sigma ? 1 : 0);
-    Mtz::Column& ecol = mtz.copy_column(e_idx, fcol, trailing_cols);
+    int fcol_idx = fcol->idx; // fcol gets invalidated in the next line
+    Mtz::Column& ecol = mtz.copy_column(e_idx, *fcol, trailing_cols);
+    fcol = &mtz.columns[fcol_idx];
     ecol.label = e_label;
     ecol.type = 'E';
     if (use_sigma) {
       assert(ecol.get_next_column_if_type('Q') != nullptr);
       (&ecol + 1)->label = "SIG" + ecol.label;
     }
+    if (!mtz.has_data())  // shouldn't happen
+      gemmi::fail("no data");
+    gemmi::Binner binner;
+    int f_count = 0;
+    for (size_t n = 0; n < mtz.data.size(); n += mtz.columns.size())
+      if (!std::isnan(mtz.data[n + fcol_idx]))
+        ++f_count;
+    int nbins = gemmi::iround(f_count / 200.);
+    if (verbose)
+      std::fprintf(stderr, "%d reflections, %d Fs, %d shells\n",
+                   mtz.nreflections, f_count, nbins);
+    binner.setup(nbins, gemmi::Binner::Method::Dstar2, gemmi::MtzDataProxy{mtz});
+    std::vector<int> bin_index = binner.get_bins(gemmi::MtzDataProxy{mtz});
     gemmi::GroupOps gops = mtz.spacegroup->operations();
-    for (size_t n = 0; n < mtz.data.size(); n += mtz.columns.size()) {
+    std::vector<double> denom(binner.size(), 0.);
+    std::vector<int> count(denom.size(), 0);
+    if (verbose)
+      std::fprintf(stderr, "Calculating E ...\n");
+    auto bin_num = bin_index.begin();
+    for (size_t n = 0; n < mtz.data.size(); n += mtz.columns.size(), bin_num++) {
       gemmi::Miller hkl = mtz.get_hkl(n);
-      int epsilon = gops.epsilon_factor(hkl);
-      double f = mtz.data[n + fcol.idx];
-      mtz.data[n + ecol.idx] = float(f / std::sqrt(epsilon));
+      double f = mtz.data[n + fcol_idx];
+      if (std::isnan(f))
+        continue;
+      double f2 = f * f / gops.epsilon_factor(hkl);
+      mtz.data[n + ecol.idx] = std::sqrt(float(f2));
+      denom[*bin_num] += f2;
+      count[*bin_num]++;
     }
-    // TODO
+    for (size_t i = 0; i < denom.size(); ++i)
+      denom[i] = std::sqrt(denom[i] / count[i]);
+    for (size_t i = 0, n = 0; n < mtz.data.size(); n += mtz.columns.size(), ++i)
+      mtz.data[n + ecol.idx] /= (float) denom[bin_index[i]];
     if (verbose)
       std::fprintf(stderr, "Writing %s ...\n", output);
     mtz.write_to_file(output);
