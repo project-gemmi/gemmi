2686e591df0c02d5c3b9307fb1301dc6a1dc8275
Marcin Wojdyr <wojdyr@gmail.com>
2023-11-30 22:13:31 +0100

    add missing header

---
+++ prog/validate_mon.h
@@ -0,0 +1,5 @@
+#include <map>
+#include "gemmi/cifdoc.hpp"
+
+void check_monomer_doc(const gemmi::cif::Document& doc, bool normal_checks, double z_score,
+                       const std::map<std::string, gemmi::cif::Block>& ccd_map, bool verbose);
