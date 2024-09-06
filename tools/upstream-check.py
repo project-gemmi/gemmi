a574cdce8a2bd5629ab0cdfec78b0c1b469f82f3
Marcin Wojdyr <wojdyr@gmail.com>
2024-09-06 15:26:14 +0200

    update fast_float to v6.1.5

--- tools/upstream-check.py
+++ tools/upstream-check.py
@@ -12,7 +12,7 @@ TAGGED_REPOS = {
     'scikit-build/scikit-build-core': 'v0.9.4',
     'taocpp/PEGTL': '2.8.3',
     'cxong/tinydir': '1.2.6',
-    'fastfloat/fast_float': 'v6.1.1',
+    'fastfloat/fast_float': 'v6.1.5',
     'madler/zlib': 'v1.3.1',
     'LLNL/shroud': 'v0.13.0',
 }
