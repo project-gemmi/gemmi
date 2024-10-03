5700b31e7b96558a80dcd925d6b1af39f8cce6f6
Marcin Wojdyr <wojdyr@gmail.com>
2024-10-03 17:30:39 +0200

    updating nanobind to 2.2 allows simplifying type_caster<SeqId::OptionalNum>
    and nanobind is compatible with Clang 8 again

--- python/meta.h
+++ python/meta.h
@@ -1,31 +1,12 @@
+#include <nanobind/stl/detail/nb_optional.h>
 
 namespace gemmi {
   std::ostream& operator<< (std::ostream& os, const Entity& ent);
 }
 
 namespace nanobind { namespace detail {
-  template<> struct type_caster<gemmi::SeqId::OptionalNum> {
-    NB_TYPE_CASTER(gemmi::SeqId::OptionalNum, optional_name(const_name("int")))
-    using Caster =  make_caster<int>;
-
-    bool from_python(handle src, uint8_t flags, cleanup_list* cleanup) noexcept {
-      if (src.is_none()) {
-        value = Value();
-        return true;
-      }
-      Caster caster;
-      if (!caster.from_python(src, flags_for_local_caster<int>(flags), cleanup))
-        return false;
-      value = caster.operator cast_t<int>();
-      return true;
-    }
-
-    static handle from_cpp(Value value, rv_policy policy, cleanup_list *cleanup) noexcept {
-      if (!value)
-        return none().release();
-      return Caster::from_cpp(*value, policy, cleanup);
-    }
-  };
+  template<> struct type_caster<gemmi::SeqId::OptionalNum>
+    : optional_caster<gemmi::SeqId::OptionalNum> {};
 }} // namespace nanobind::detail
 
 NB_MAKE_OPAQUE(std::vector<gemmi::Helix>)
