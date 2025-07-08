c16f3160886873cb50d52e7d339b9796af93d997
Marcin Wojdyr <wojdyr@gmail.com>
2025-07-08 07:37:13 +0200

    tests/test_fetch.py - skip rather than fail on connection problems

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import unittest
+import urllib.error
+import http.client
 
 import gemmi
 import gemmi.fetch
@@ -11,6 +13,11 @@ class TestFetch(unittest.TestCase):
         cell = gemmi.read_structure(full_path('5i55.cif')).cell
         for site in "REJ":
             for use_cif in [True, False]:
-                st = gemmi.fetch.read_structure_with_code('5i55', pdb_site=site,
-                                                          use_cif=use_cif)
-                self.assertEqual(cell, st.cell)
+                try:
+                    st = gemmi.fetch.read_structure_with_code('5i55', pdb_site=site,
+                                                              use_cif=use_cif)
+                    self.assertEqual(cell, st.cell)
+                except urllib.error.URLError as e:
+                    self.skipTest(f"Cannot connect to {site} ({e})")
+                except http.client.RemoteDisconnected as e:
+                    self.skipTest(f"Couldn't fetch file form {site}: ({e})")
