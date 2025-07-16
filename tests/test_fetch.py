e92ca194ea53ea9d8d20b3716faaa9541074136c
Marcin Wojdyr <wojdyr@gmail.com>
2025-07-16 13:42:55 +0200

    rearrange tests/test_fetch.py

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -9,15 +9,26 @@ import gemmi.fetch
 from common import full_path
 
 class TestFetch(unittest.TestCase):
-    def test_fetch(self):
+    def fetch(self, site):
         cell = gemmi.read_structure(full_path('5i55.cif')).cell
-        for site in "REJ":
-            for use_cif in [True, False]:
-                try:
-                    st = gemmi.fetch.read_structure_with_code('5i55', pdb_site=site,
-                                                              use_cif=use_cif)
-                    self.assertEqual(cell, st.cell)
-                except urllib.error.URLError as e:
-                    self.skipTest(f"Cannot connect to {site} ({e})")
-                except http.client.RemoteDisconnected as e:
-                    self.skipTest(f"Couldn't fetch file form {site}: ({e})")
+        for use_cif in [True, False]:
+            try:
+                st = gemmi.fetch.read_structure_with_code('5i55', pdb_site=site,
+                                                          use_cif=use_cif)
+                self.assertEqual(cell, st.cell)
+            except urllib.error.URLError as e:
+                self.skipTest(f"Cannot connect to {site} ({e})")
+            except http.client.RemoteDisconnected as e:
+                self.skipTest(f"Couldn't fetch file form {site}: ({e})")
+    @unittest.skipIf(__name__ != '__main__', "Skip during unittest discover")
+    def test_rcsb(self):
+        self.fetch('R')
+    @unittest.skipIf(__name__ != '__main__', "Skip during unittest discover")
+    def test_pdbe(self):
+        self.fetch('E')
+    @unittest.skipIf(__name__ != '__main__', "Skip during unittest discover")
+    def test_pdbj(self):
+        self.fetch('J')
+
+if __name__ == '__main__':
+    unittest.main()
