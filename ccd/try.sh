adececc867e5884980810c11dfbd93e25c364b05
Marcin Wojdyr <wojdyr@gmail.com>
2026-01-26 08:56:29 +0100

    tweak ccd/try.sh:

--- ccd/try.sh
+++ ccd/try.sh
@@ -7,11 +7,18 @@ for x in "$@"; do
   code=${x^^}
   files+=("orig/$code.cif")
 done
-../build/gemmi drg -v --output-dir=gemmi "${files[@]}" 2>drg.log
+../build/gemmi drg --typeOut -v --output-dir=gemmi "${files[@]}" 2>drg.log
 
+echo ====================  COMPARING acedrg_type ===========
 for x in "$@"; do
   code=${x^^}
-  # echo ========================$code========================
+  echo ====================  $code ===========
+  ../build/gemmi mondiff acedrg/${code}_atomTypes.cif gemmi/$code.cif | grep acedrg_type
+done
+echo ====================  COMPARING restraints ===========
+for x in "$@"; do
+  code=${x^^}
+   echo ========================$code========================
   [ -f orig/$code.cif ] || ./use.py $code
   echo ====================  $code ===========
   #echo ./build/gemmi mondiff ./ccd/acedrg/$code.cif ./ccd/gemmi/$code.cif
