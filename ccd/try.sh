1e496e708c8ba6cee06787f2e486e66a51e87982
Marcin Wojdyr <wojdyr@gmail.com>
2026-01-28 00:39:40 +0100

    tweak acedrg_tables testing scripts

--- ccd/try.sh
+++ ccd/try.sh
@@ -2,6 +2,7 @@
 
 #:rm -f gemmi/*.cif
 ../run-tests.sh G
+echo START:
 
 for x in "$@"; do
   code=${x^^}
@@ -13,7 +14,7 @@ echo ====================  COMPARING acedrg_type ===========
 for x in "$@"; do
   code=${x^^}
   echo ====================  $code ===========
-  ../build/gemmi mondiff acedrg/${code}_atomTypes.cif gemmi/$code.cif | grep acedrg_type
+  (  ../build/gemmi mondiff acedrg/${code}_atomTypes.cif gemmi/$code.cif | grep acedrg_type) ||:
 done
 echo ====================  COMPARING restraints ===========
 for x in "$@"; do
@@ -22,5 +23,6 @@ for x in "$@"; do
   [ -f orig/$code.cif ] || ./use.py $code
   echo ====================  $code ===========
   #echo ./build/gemmi mondiff ./ccd/acedrg/$code.cif ./ccd/gemmi/$code.cif
-  ../build/gemmi mondiff ./acedrg/$code.cif ./gemmi/$code.cif 
+  ../build/gemmi mondiff ./acedrg/$code.cif ./gemmi/$code.cif ||:
 done
+echo END
