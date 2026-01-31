b1f2c2ba071a2fcd1bdac2521a26bed907a32313
Marcin Wojdyr <wojdyr@gmail.com>
2026-01-31 11:20:27 +0100

    tweak try.sh

--- ccd/try.sh
+++ ccd/try.sh
@@ -2,18 +2,17 @@
 
 #:rm -f gemmi/*.cif
 ../run-tests.sh G
-echo START:
 
 for x in "$@"; do
   code=${x^^}
   files+=("orig/$code.cif")
 done
-../build/gemmi drg --typeOut -v --output-dir=gemmi "${files[@]}" 2>drg.log
+../build/gemmi drg --typeOut -v --output-dir=gemmi "${files[@]}" 2>drg.log ||:
 
 for x in "$@"; do
   code=${x^^}
    echo ========================$code========================
   [ -f orig/$code.cif ] || ./use.py $code
-  ../build/gemmi mondiff ./acedrg/$code.cif ./gemmi/$code.cif ||:
+  ../build/gemmi mondiff --only=atb./acedrg/$code.cif ./gemmi/$code.cif ||:
 done
-echo END
+echo "Scipt ended successfully!!! "
